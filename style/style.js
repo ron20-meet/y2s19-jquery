d192c83930969198372fdb2b14e6e1b085e1e654
myname21-meet <myname21@meet.mit.edu>
2019-07-24 07:13:16 -0400

    day4lesson2

--- style/style.js
+++ style/style.js
@@ -4,15 +4,16 @@
 
 function updateSize(newSize) {
   // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
+  $("#logo").css("width" , newSize)
+
 }
 
 
 function updateLeftPosition(newX) {
-	// FINISH ME
-
+	$("#logo").css("left" , newX)
 }
 
 function updateTopPosition(newY) {
-	// FINISH ME
+	$("#logo").css("tops" , newY)
 }
 
